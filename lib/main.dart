56906e7611b0eb36c12aecfe652290637d141560
Oleksandr Romaniuk <oleksandr.romaniuk@protonmail.com>
2022-10-25 20:28:40 +0300

    step-5

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:developer' as devtools show log;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:justmynotes/constants/routes.dart';
 import 'package:justmynotes/firebase_options.dart';
 import 'package:justmynotes/views/login_view.dart';
 import 'package:justmynotes/views/register_view.dart';
@@ -19,9 +20,9 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login': (context) => const LoginView(),
-        '/register': (context) => const RegisterView(),
-        '/notes': (context) => const NotesView(),
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
       },
     ),
   );
@@ -49,7 +50,7 @@ class HomePage extends StatelessWidget {
                 return const VerifyEmailView();
               }
             } else {
-              devtools.log('User in not log in...');
+              devtools.log('User is not log in...');
               return const LoginView();
             }
           default:
@@ -87,7 +88,7 @@ class _NotesViewState extends State<NotesView> {
                     if (!mounted) return;
                     devtools.log('User log out');
                     Navigator.of(context).pushNamedAndRemoveUntil(
-                      '/login',
+                      loginRoute,
                       (_) => false,
                     );
                   }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,7 @@ import 'dart:developer' as devtools show log;
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:justmynotes/constants/routes.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -65,7 +66,7 @@ class _LoginViewState extends State<LoginView> {
                 );
                 if (!mounted) return;
                 Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/notes',
+                  notesRoute,
                   (route) => false,
                 );
                 devtools.log('User log in successfully');
@@ -82,7 +83,7 @@ class _LoginViewState extends State<LoginView> {
           TextButton(
             onPressed: () {
               Navigator.of(context).pushNamedAndRemoveUntil(
-                '/register',
+                registerRoute,
                 (route) => false,
               );
             },
